6f66fe34ba8c4c1526da18392cc55e1cdd643232
Rodolfo Coprodutor <rodolfocoprodutor@gmail.com>
2022-07-21 19:37:00 -0300

    diposnibilizei a imagem do perfil de forma off

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("static").then(cache => {
-      return cache.addAll(["./", "./src/main.css", "./assets/logo192.png", "./assets/logo512.png"])
+      return cache.addAll(["./", "./src/main.css", "./assets/logo192.png", "./assets/logo512.png", "./assets/profile-aircard.png"])
     })
   )
 });
